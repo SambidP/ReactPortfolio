ee2c291dc0a8c79a1ad0a8f140496e73ce9e8ddf
SambidP <sambidp@gmail.com>
2025-01-16 18:50:33 +0545

    AnimatedLetters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,58 @@
-import { Link } from "react-router-dom";
-import LogoTitle from "../../assets/images/logo-s.png";
-import './index.scss';
+import './index.scss'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a', 'm', 'b', 'i', 'd']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, <br/> I'm
-                <img src= {LogoTitle} alt="developer" />
-                ambid
-                <br />
-                web developer
-                </h1>
-                <h2>Laravel Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}> H </span>
+          <span className={`${letterClass} _12`}> i, </span>
+          <br />
+          <span className={`${letterClass} _13`}> I </span>
+          <span className={`${letterClass} _14`}> 'm </span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Laravel Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home
