ff12b38e40ada59d8aa0bd921747eed719732b6a
SambidP <sambidp@gmail.com>
2025-02-22 18:04:18 +0545

    final changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,18 @@ import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const customIcon = new L.Icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Default size
+  iconAnchor: [12, 41], // Positioning
+  popupAnchor: [1, -34], // Popup positioning
+  shadowSize: [41, 41],
+});
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -101,10 +113,9 @@ const Contact = () => {
         </div>
         <div className='map-wrap'>
           <MapContainer center={[27.75925, 85.32995]} zoom={13}>
-          {/* <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" /> */}
             <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-            <Marker position={[27.75925, 85.32995]} >
-            <Popup></Popup>
+            <Marker position={[27.75925, 85.32995]} icon={customIcon} >
+            <Popup>Sambid's home, come for coffee XD</Popup>
             </Marker >
           </MapContainer>
         </div>
